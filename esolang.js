eabaadfc2883e8edf6ed74da2073d294e02ce332
Robin Miller <rmillerpdx@gmail.com>
2017-09-14 22:07:41 -0700

    commit before i ruin it

--- esolang.js
+++ esolang.js
@@ -5,36 +5,40 @@ function interpreter(code, tape) {
     tapeArray = tape.split('');
     tapeIndex = 0;
     jumpIndex = [];
+    code = code.split('');
+    codeIndex = 0;
 
     function flip (array,index){
       array[index] === '0' ? array[index] = '1' : array[index] = '0';
     }
 
-    code.split('').forEach(function(codeElement,index){ //Refactor to while loop for (code[index] < code.length)
-      debugger
-      switch (codeElement){
-        case '*': flip(tapeArray,tapeIndex); break;
-        case '>': tapeIndex++; break;
-        case '<': tapeIndex--; break;
-        case '[': jumpIndex.push(tapeIndex); tapeIndex++; break;
-        case ']': if (tapeArray[tapeIndex] === '0') {
-          tapeIndex = jumpIndex[0];
-          jumpIndex.shift();
+
+    while (codeIndex < code.length){
+      // debugger
+      switch (code[codeIndex]){
+        case '*': flip(tapeArray,tapeIndex); codeIndex++; break;
+        case '>': tapeIndex++; codeIndex++; break;
+        case '<': tapeIndex--; codeIndex++;break;
+        case '[': jumpIndex.push(codeIndex); tapeIndex++; codeIndex++; break;
+        case ']': if (tapeArray[tapeIndex] === '1') {
+          codeIndex = jumpIndex[jumpIndex.length - 1];
+          jumpIndex.pop();
           break;
          } else {
-           tapeIndex++; 
+           codeIndex++;
            break;
         }
       }//end of switch
-    }//end of forEach
-  )
+      codeIndex++;
+    }
+    console.log(tapeArray.join(''));
     return (tapeArray.join(''));
 
   }
 
 
 
-  interpreter(">>[>>>*]<*<<*", "00101100");
+  interpreter("*[>*]", "0000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000");
   //"00000000")
 
 //   Test.describe("Your Interpreter", function () {
