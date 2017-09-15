d202325b59317dd158efa64acb840289ae6378e9
Robin Miller <rmillerpdx@gmail.com>
2017-09-15 09:38:41 -0700

    fix loop bug

--- esolang.js
+++ esolang.js
@@ -1,34 +1,32 @@
 function interpreter(code, tape) {
     // Implement your interpreter here
+
     console.log('code is',code);
     console.log('tape is',tape);
     tapeArray = tape.split('');
     tapeIndex = 0;
-    jumpIndex = [];
     code = code.split('');
     codeIndex = 0;
+    jumpIndex = [];
 
     function flip (array,index){
       array[index] === '0' ? array[index] = '1' : array[index] = '0';
     }
+debugger
+    while (tapeIndex !== tape.length){
 
-
-    while (codeIndex < code.length){
-      // debugger
       switch (code[codeIndex]){
-        case '*': flip(tapeArray,tapeIndex); codeIndex++; break;
-        case '>': tapeIndex++; codeIndex++; break;
-        case '<': tapeIndex--; codeIndex++;break;
-        case '[': jumpIndex.push(codeIndex); tapeIndex++; codeIndex++; break;
+        case '*': flip(tapeArray,tapeIndex);  break;
+        case '>': tapeIndex++;  break;
+        case '<': tapeIndex--; break;
+        case '[': jumpIndex.push(codeIndex);  break;
         case ']': if (tapeArray[tapeIndex] === '1') {
-          codeIndex = jumpIndex[jumpIndex.length - 1];
+          codeIndex = jumpIndex[jumpIndex.length - 1] - 1;
           jumpIndex.pop();
           break;
-         } else {
-           codeIndex++;
-           break;
-        }
+         } 
       }//end of switch
+      debugger
       codeIndex++;
     }
     console.log(tapeArray.join(''));
@@ -38,9 +36,8 @@ function interpreter(code, tape) {
 
 
 
-  interpreter("*[>*]", "0000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000");
-  //"00000000")
-
+  interpreter("*[>*]", "00000000000000000000000000000000000000");
+  "11111000000000000000000000000000000000"
 //   Test.describe("Your Interpreter", function () {
 //     Test.it("should work for some example test cases", function () {
 //       // Flips the leftmost cell of the tape
