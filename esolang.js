034dc7a6087ef3c283e41251b20858598daa2c13
Robin Miller <rmillerpdx@gmail.com>
2017-09-27 14:06:50 -0700

    commit before I break it

--- esolang.js
+++ esolang.js
@@ -17,12 +17,13 @@ function interpreter(code, tape) {
     }
 
 debugger
-    while (tapeIndex !== tape.length && tapeIndex < tape.length && codeIndex > -1 && codeIndex < code.length){
+    while (tapeIndex < tape.length && tapeIndex < tape.length && codeIndex > -1 && codeIndex < code.length){
 
       switch (code[codeIndex]){
         case '*': flip(tapeArray,tapeIndex);  break;
         case '>': tapeIndex++;  break;
         case '<': tapeIndex--; break;
+        //TODO: fix
         case '[': tapeArray[tapeIndex] === '0' ? codeIndex = code.indexOf(']', codeIndex) + 1 : jumpIndex.push(codeIndex); break;
         case ']': if (tapeArray[tapeIndex] === '1') {
           codeIndex = jumpIndex[jumpIndex.length - 1] - 1;
