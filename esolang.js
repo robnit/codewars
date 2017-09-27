4a7eadc1f36a639a255305c7b74c0a0896649ddf
Robin Miller <rmillerpdx@gmail.com>
2017-09-27 13:51:49 -0700

    fix while conditional

--- esolang.js
+++ esolang.js
@@ -1,5 +1,4 @@
 function interpreter(code, tape) {
-    // Implement your interpreter here
 
     console.log('code is',code);
     console.log('tape is',tape);
@@ -12,14 +11,19 @@ function interpreter(code, tape) {
     function flip (array,index){
       array[index] === '0' ? array[index] = '1' : array[index] = '0';
     }
+
+    function leftBracketCounter (currentIndex, rightBracketIndex){
+      code.splice(currentIndex + 1, rightBracketIndex).filter(a => { return a === '[' } ).length;
+    }
+
 debugger
-    while (tapeIndex !== tape.length){
+    while (tapeIndex !== tape.length && tapeIndex < tape.length && codeIndex > -1 && codeIndex < code.length){
 
       switch (code[codeIndex]){
         case '*': flip(tapeArray,tapeIndex);  break;
         case '>': tapeIndex++;  break;
         case '<': tapeIndex--; break;
-        case '[': jumpIndex.push(codeIndex);  break;
+        case '[': tapeArray[tapeIndex] === '0' ? codeIndex = code.indexOf(']', codeIndex) + 1 : jumpIndex.push(codeIndex); break;
         case ']': if (tapeArray[tapeIndex] === '1') {
           codeIndex = jumpIndex[jumpIndex.length - 1] - 1;
           jumpIndex.pop();
@@ -29,15 +33,20 @@ debugger
       debugger
       codeIndex++;
     }
-    console.log(tapeArray.join(''));
+    console.log('answer is',tapeArray.join(''));
     return (tapeArray.join(''));
 
   }
+  // TODO: Make case ']' accept nested brackets
+// [esfef [sdfsd [sdfsdfsd] sdfs] sdf]
 
 
+  interpreter("*[>[*]*>]>", "00000000000000000000000000000000000000");
 
-  interpreter("*[>*]", "00000000000000000000000000000000000000");
-  "11111000000000000000000000000000000000"
+//   [ - Jump past matching ] if value at current cell is 0
+//   ] - Jump back to matching [ (if value at current cell is nonzero)
+
+
 //   Test.describe("Your Interpreter", function () {
 //     Test.it("should work for some example test cases", function () {
 //       // Flips the leftmost cell of the tape
---
+++ under29.js
@@ -0,0 +1,6 @@
+// PRO TIP: USE THE SORT METHOD
+//RECURSIVE SOLUTION f= a => a[0] ? [a.pop()].concat(f(a)) : a;
+
+
+
+f = a => a.sort(function(x,y){return a.indexOf[x]-a.indexOf[y] });
