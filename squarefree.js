b79ca7cd9508c9522e01e6c0797f31a3b1825a1c
Robin Miller <rmillerpdx@gmail.com>
2018-01-11 16:06:07 -0800

    squareFree functional but unoptimized

---
+++ squarefree.js
@@ -0,0 +1,19 @@
+function squareFreePart(n) {
+
+  function hasSquareFactors(num) {
+    let i = Math.ceil(num/2);
+    while(true) {
+      if (i === 1) return false;
+      if ( (Math.sqrt(i) % 1 === 0) && num%i === 0) return true;
+      i--;
+    }
+  }
+  
+  let divisor = (n < 3) ? n : Math.ceil(n/2);
+  
+  while (true) {
+    if (divisor === 0) return null;
+    if ( !hasSquareFactors(divisor) && n%divisor === 0 ) return divisor;
+    divisor--;
+  }
+}
